34bdf9a6b6ec09a344207200244d7510bdc1640e
raiqa <rasoolraiqa@gmail.com>
2021-11-26 07:20:58 +0500

    Working on menu

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const hamberger_icon=document.getElementsByClassName('menu-hamberger-icon');
 const cross_icon=document.getElementsByClassName('menu-cross-icon');
 const mobile_menu_side_background=document.getElementsByClassName('mobile-menu-side-background');
-const mobile_menu=document.getElementsByClassName('main-menu');
 
 function mobile_menu_appear(){
-    mobile_menu_side_background.classList.add("mobile-menu-side-background-active");
+    mobile_menu_side_background[0].classList.add("mobile-menu-side-background-active");
 }
-hamberger_icon[0].addEventListener(onclick,mobile_menu_appear);
+hamberger_icon[0].addEventListener("click",mobile_menu_appear);
