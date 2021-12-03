639f427d9e6f13296c51d82a13d1f4d0df530caf
raiqa <rasoolraiqa@gmail.com>
2021-12-03 10:42:00 +0500

    Desktop version CSS done

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// ==================MOBILE MENU====================
+
 const hamberger_icon=document.getElementsByClassName('menu-hamberger-icon');
 const cross_icon=document.getElementsByClassName('menu-cross-icon');
 const mobile_menu_side_background=document.getElementsByClassName('mobile-menu-side-background');
@@ -11,3 +13,30 @@ function mobile_menu_disappear()
     mobile_menu_side_background[0].classList.remove("mobile-menu-side-background-active");
 }
 cross_icon[0].addEventListener("click",mobile_menu_disappear);
+
+
+// ====================Main Image part slider==================
+const main_image=document.getElementsByClassName("main-image");
+const thumbnail_image=document.getElementsByClassName("thumbnail-image");
+
+function slide_show(x)
+{
+        x.classList.add("thumbnail-image-hover");
+        for(j=0;i<thumbnail_image.length;j++)
+        {
+        if (thumbnail_image.indexOf(x)==j)
+        continue;
+        thumbnail_image[j].classList.remove("thumbnail-image-hover");
+        }
+
+        let i=thumbnail_image.indexOf(x);
+        main_image[i].style.display="block";
+        for(g in main_image)
+        {   if(main_image(indexOf(g)==i))
+            continue;
+            g.style.display="none";
+        }
+        
+}
+
+thumbnail_image.forEach(x => x.addEventListener("onclick",slide_show(x)));
